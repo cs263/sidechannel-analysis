c5335712519fdf9b4ea6f49a5710b05931ed00c0
tsbrennan1 <tegan@umail.ucsb.edu>
2017-04-20 14:24:21 -0700

    Added secure and insecure version of string comparison in java

---
+++ tests/password/PasswordConstantTime.java
@@ -0,0 +1,27 @@
+package tests.password;
+
+public class PasswordConstantTime {
+
+    private static String password;
+
+    public static boolean checkPassword(String s){
+    	if (s.length() != password.length()){
+            return false;
+    	}
+        int res = 0;
+        for (int i = 0; i < s.length(); i++){
+            res |= s.charAt(i) ^ password.charAt(i);
+        }
+        return res == 0;
+    }
+
+    public static void main(String[] args){
+    	if (args.length != 2){
+    		System.out.println("Please provide two arguments -- a password and a guess");
+    		System.exit(0); 
+    	}
+        password = args[0];
+        boolean res = checkPassword(args[1]);
+        System.out.println(res);
+    }
+}
---
+++ tests/password/PasswordInsecure.java
@@ -0,0 +1,28 @@
+package tests.password;
+
+public class PasswordInsecure {
+
+    private static String password;
+
+    public static boolean checkPassword(String s){
+    	if (s.length() != password.length()){
+    		return false;
+    	}
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != password.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+    	if (args.length != 2){
+    		System.out.println("Please provide two arguments -- a password and a guess");
+    		System.exit(0); 
+    	}
+        password = args[0];
+        boolean res = checkPassword(args[1]);
+        System.out.println(res);
+    }
+}
