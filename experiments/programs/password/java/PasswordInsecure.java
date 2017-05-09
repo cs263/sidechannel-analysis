366bd1e9aaa1baf22c222b49a81bc7d0773fa922
Miroslav Gavrilov <gavrilov.miroslav@gmail.com>
2017-05-08 17:47:26 -0700

    Typing change in password insecure java file

--- experiments/programs/password/java/PasswordInsecure.java
+++ experiments/programs/password/java/PasswordInsecure.java
@@ -5,19 +5,19 @@ public class PasswordInsecure {
     private static String password;
 
     public static boolean checkPassword(String s){
-    	if (s.length() != password.length()){
+    	if (s.length() != password.length()) {
     		return false;
     	}
-        for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) != password.charAt(i)){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != password.charAt(i)) {
                 return false;
             }
         }
         return true;
     }
 
-    public static void main(String[] args){
-    	if (args.length != 2){
+    public static void main(String[] args) {
+    	if (args.length != 2) {
     		System.out.println("Please provide two arguments -- a password and a guess");
     		System.exit(0); 
     	}
