11e04b08e374d8d7a66cccf31aa3ecd75e8ed47c
Gavrilov Miroslav <gavrilov.miroslav@gmail.com>
2017-05-24 01:52:05 -0700

    Added stack-based path-constraints

--- experiments/tools/conflow/src/main/scala/graphs.scala
+++ experiments/tools/conflow/src/main/scala/graphs.scala
@@ -18,7 +18,7 @@ package conflow {
 		object Graph {
 			def dot[T, S](graph: Graph[T, S]): String = {
 				val parts = graph.map { case n@Node(k, in, vs) =>
-					vs.map { case (s, i) => s"\tBB${k} -> BB${i}" }.mkString(";\n") //+ "[label=\"" + s + "\"]" }.mkString(";\n")
+					vs.map { case (s, i) => s"\t${k} -> ${i}" } + "[label=\"" + s + "\"]" }.mkString(";\n")
 				}.mkString(";\n")
 
 				s"digraph G {\n${parts}}"
