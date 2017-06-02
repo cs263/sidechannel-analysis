298e6cbe168c31bea7bc59e31333a696520ac153
tegan <tegan@umail.ucsb.edu>
2017-06-02 15:39:19 -0700

    Added another variant that may or may not be insecure based on compiler optimizations

---
+++ experiments/programs/password/java/PossiblyInsecureTwo.java
@@ -0,0 +1,38 @@
+package experiments.programs.password.java;
+
+public class PossiblyInsecure {
+
+    private static String password;
+
+    public static boolean checkPassword(String s){
+        boolean flag = true; 
+        boolean flag2 = true; 
+    	if (s.length() != password.length()){
+            return false;
+    	}
+        for (int i = 0; i < s.length(); i++){
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException ex){
+                Thread.currentThread().interrupt();
+            }
+            if (s.charAt(i) != password.charAt(i)){
+                flag = false; 
+            }
+            else{
+                flag2 = !flag2;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args){
+    	if (args.length != 2){
+    		System.out.println("Please provide two arguments -- a password and a guess");
+    		System.exit(0); 
+    	}
+        password = args[0];
+        boolean res = checkPassword(args[1]);
+        System.out.println(res);
+    }
+}
