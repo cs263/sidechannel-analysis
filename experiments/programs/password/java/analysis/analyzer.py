9e69c165e234a55d2afe4f5ae7ca9894a31f4646
tsbrennan1 <tegan@umail.ucsb.edu>
2017-06-03 02:06:39 -0700

    Added a python script to get a quick overview of results

---
+++ experiments/programs/password/java/analysis/analyzer.py
@@ -0,0 +1,31 @@
+import sys
+import numpy as np 
+
+#Very hacked code to get quick results. Just pipe the output of your run java V=.... run > file1 and run python analyzer.py file1. 
+results = {}
+
+def update_res(num, word):
+	k = word[0]
+	if k in results:
+		results[k].append(int(num))
+	else:
+		results[k] = [int(num)]
+
+def process():
+	for key in results.keys():
+		results[key] = np.mean(results[key])
+
+if __name__ == "__main__":
+	input_file = sys.argv[1]
+	for i,line in enumerate(open(input_file)):
+		if i < 3:
+			if i == 1:
+				print("first letter is " + line[0])
+			continue
+		in_out = line.split()
+		if in_out:
+			num = in_out[0].strip(',')
+			word = in_out[1]
+			update_res(num, word)
+	process()
+	print(results)
