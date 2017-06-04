28fa5031a9591d88365d896d30b936efc3aafc96
tsbrennan1 <tegan@umail.ucsb.edu>
2017-06-03 17:17:07 -0700

    Added code to plot the results

--- experiments/programs/password/java/analysis/analyzer.py
+++ experiments/programs/password/java/analysis/analyzer.py
@@ -1,9 +1,12 @@
 import sys
 import numpy as np 
 import operator
+import numpy as np
+import matplotlib.pyplot as plt 
 
 #Very hacked code to get quick results. Just pipe the output of your run java V=.... run > file1 and run python analyzer.py file1. 
 results = {}
+secret = None
 
 def update_res(num, word):
 	k = word[0]
@@ -11,6 +14,18 @@ def update_res(num, word):
 		results[k].append(int(num))
 	else:
 		results[k] = [int(num)]
+
+def plot():
+	x = []
+	y = []
+	for key in results.keys():
+		if key!=secret:
+			y.append(results[key])
+			x.append(1)
+
+	plt.scatter(x,y,c='blue')
+	plt.scatter(1,results[secret],c='red')
+	plt.show()
 
 def process():
 	for key in results.keys():
@@ -25,6 +40,7 @@ if __name__ == "__main__":
 		if i < 3:
 			if i == 1:
 				print("first letter is " + line[0])
+				secret = line[0]
 			continue
 		in_out = line.split()
 		if in_out:
@@ -34,3 +50,4 @@ if __name__ == "__main__":
 	process()
 	sorted_res = sorted(results.items(), key=operator.itemgetter(1), reverse =True)
 	print(sorted_res)
+	plot()
