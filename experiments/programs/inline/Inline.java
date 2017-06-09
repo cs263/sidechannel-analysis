6e792c05bbf03628155b67ec902aed4be77c962c
tegan <tegan@umail.ucsb.edu>
2017-06-08 18:42:43 -0700

    Added example demonstrating how inlining can produce side channels

---
+++ experiments/programs/inline/Inline.java
@@ -0,0 +1,62 @@
+package tests.inline;
+import java.nio.file.*;
+import java.util.*;
+import java.security.*;
+import java.io.*;
+import java.util.Random;
+
+public class Inline  {
+    static SecureRandom rnd = new SecureRandom();
+    static int threshold = 25;
+    private static StringBuffer buf = new StringBuffer();    
+        private static long t;
+
+
+
+    public static void main(String[] args) throws IOException {
+        int guess;
+        for (int i = 0; i < 100000; i++){
+            guess = rnd.nextInt(100);
+            t = System.nanoTime();
+            check(guess);
+            t = System.nanoTime() - t;
+            System.out.print((guess<threshold) + " " );
+            System.out.println(t);
+            /*buf.append(guess < threshold);
+            buf.append(" ");
+            buf.append(t);
+            buf.append("\n");*/
+
+        }
+                System.out.println(buf.toString());
+
+    }
+
+    public static int doWork(int a){
+        a+=4;
+        return a+a;
+        
+
+    }
+
+    public static int doWork2(int b){
+        b+=4; 
+        return b+b;
+    }
+
+    public static boolean check(int guess) {
+        if (guess < threshold){
+            for (int i =0; i < 100000; i++){
+                doWork(i);
+            }
+
+        }
+        else{
+            for (int i = 0; i < 100000; i++){
+                doWork2(i);
+            }
+        }
+        return false;
+    }
+}
+
